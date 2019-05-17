9a7d25c141fc67cec130ecfbf2597eb30a4c3989
thirupathi.g4795@gmail.com <thirupathi.g4795@gmail.com>
2019-05-17 15:48:08 +0530

    Add project files.

---
+++ SeleniumMSBuild1/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumMSBuild1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            string title=driver.Title;
+            Console.WriteLine("Title is: "+title);
+            driver.Quit();
+        }
+    }
+}
