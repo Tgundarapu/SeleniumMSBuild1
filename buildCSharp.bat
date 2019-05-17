40b2956541e17a3ee0fe3f576375cd6fb48ce477
Tgundarapu <50095040+Tgundarapu@users.noreply.github.com>
2019-05-17 18:41:01 +0530

    Add files via upload

---
+++ buildCSharp.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "build.xml"
+"E:\Ratan\Visual_Studio_Project\SeleniumMSBuild1\SeleniumMSBuild1\bin\Debug\SeleniumMSBuild1.exe"
+pause
